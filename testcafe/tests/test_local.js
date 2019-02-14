fab8d05a011b10a0b5c9095cf7d79ec5505bb439
franbuehler <franziska.buehler.schmocker@gmail.com>
2019-02-14 12:02:41 +0100

    test_local with dev tests

---
+++ testcafe/tests/test_local.js
@@ -0,0 +1,66 @@
+// Normal Pixi Application Tests against Pixi directly for CI Pipeline
+import { Selector } from 'testcafe';
+
+fixture`Getting Started`
+    .page `http://192.168.1.104:8000/register`;
+
+test('Register User', async t => {
+    await t
+        .typeText('input#email', 'testuser@pixi.owasp')
+        .typeText('input#password', 'testpw')
+        .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+        //After registration the search pixi field should be there
+        //.expect('#search_query')
+});
+
+test('Login User', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+});
+
+test('Search String', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+        //After login we search for Lunch
+        .typeText('#search_query', 'Lunch')
+        .pressKey('enter')
+        //.expect('body > div > div:nth-child(12) > div > div.card-deck')
+});
+
+test('Click About', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+        //After login we click About
+	.click('body > div > div:nth-child(1) > div.col-md-7 > ul > li:nth-child(1) > a')
+        //.expect('body > div > div:nth-child(12) > div > div.card-deck')
+});
+
+test('Click My Profile and delete Picture', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+        //After login we click My Profile
+	.click('body > div > div:nth-child(1) > div.col-md-7 > ul > li:nth-child(3) > a')
+        //After clicking My Profile we delete first Photo
+	.click('body > div > div:nth-child(5) > div > div > div:nth-child(1) > div > a')
+});
+
+test('Logout User', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+	.click('body > div > div:nth-child(1) > div.col-md-7 > ul > li:nth-child(4) > a')
+});
