33f1774ad4fb620aafd10beab8d27eb5536a1c49
franbuehler <franbuehler@gmail.com>
2019-04-08 13:39:28 +0200

    Debugging

--- testcafe/tests/testwaf.js
+++ testcafe/tests/testwaf.js
@@ -6,7 +6,7 @@ fixture`Getting Started`
 
 test('Register User WITH CRS', async t => {
     await t
-        .typeText('input#email', 'test>')
+        .typeText('input#email', 'test@test>')
         .typeText('input#password', '<script>alert("My evil WAF Test")</script>')
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
         //After registration the search pixi field should be there
