7833ae166edc04d09a499778e890fdb01a239b01
franbuehler <franziska.buehler.schmocker@gmail.com>
2019-10-26 14:47:12 +0200

    Changed to official Docker image

--- testcafe/tests/mongodbcrs.js
+++ testcafe/tests/mongodbcrs.js
@@ -2,7 +2,7 @@
 import { Selector } from 'testcafe';
 
 fixture`Getting Started`
-    .page `http://172.17.0.2:443/register`;
+    .page `https://172.17.0.2:443/register`;
 
 test('Register User WITH CRS', async t => {
     await t
